390d488b88e4b1c3502da790493dfe38214fad7b
TomasAybar <aybar.t4@gmail.com>
2022-11-26 21:43:40 -0300

    url optimizadas para generar parametros

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interfaces';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interfaces';
 export class GifsService {
 
   private apiKey: string = 'Fm1VDmxbiArxwi7PdCb6MVq3y1AY1GUL';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
   public resultados: Gif[] = [];
 
@@ -50,8 +51,12 @@ export class GifsService {
 
     }
 
+    const params = new HttpParams()
+      .set('apiKey', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}z&limit=10`)
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search?`, { params: params })
       .subscribe((resp) => {
 
         console.log(resp.data);
